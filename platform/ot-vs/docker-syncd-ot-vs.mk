4f638e75d706d66baec92e31b038896ce6c3a8e2
zhengweitang <zhengweitang.zwt@alibaba-inc.com>
2024-06-13 20:38:52 +0800

    upgrade config engine from buster to bullseye

--- platform/ot-vs/docker-syncd-ot-vs.mk
+++ platform/ot-vs/docker-syncd-ot-vs.mk
@@ -1,7 +1,7 @@
 # docker image for vs syncd
 
 DOCKER_SYNCD_PLATFORM_CODE = ot-vs
-include $(PLATFORM_PATH)/../template/docker-syncd-base.mk
+include $(PLATFORM_PATH)/../template/docker-syncd-bullseye.mk
 
 $(DOCKER_SYNCD_BASE)_DEPENDS += $(SYNCD_OT_VS)
 
