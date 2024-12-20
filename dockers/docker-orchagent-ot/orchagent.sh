1fb376cbd571011a2744503f0e559821a7c69cf0
zhengweitang <zhengweitang.zwt@alibaba-inc.com>
2024-12-20 17:02:59 +0800

    fix syncd and otss runtime error

--- dockers/docker-orchagent-ot/orchagent.sh
+++ dockers/docker-orchagent-ot/orchagent.sh
@@ -8,16 +8,10 @@ export platform=$(echo $OTSS_VARS | jq -r '.asic_type')
 
 # Create a folder for OTSS record files
 mkdir -p /var/log/otss
-ORCHAGENT_ARGS="-d /var/log/otss "
+ORCHAGENT_ARGS=" "
 
 # Set orchagent pop batch size to 8192
 ORCHAGENT_ARGS+="-b 8192 "
-
-# Set synchronous mode if it is enabled in CONFIG_DB
-SYNC_MODE=$(echo $OTSS_VARS | jq -r '.synchronous_mode')
-if [ "$SYNC_MODE" == "enable" ]; then
-    ORCHAGENT_ARGS+="-s "
-fi
 
 # Check if there is an "asic_id field" in the DEVICE_METADATA in configDB.
 #"DEVICE_METADATA": {
@@ -25,19 +19,11 @@ fi
 #        ....
 #        "asic_id": "0",
 #    }
-#},
-# ID field could be integers just to denote the asic instance like 0,1,2...
-# OR could be PCI device ID's which will be strings like "03:00.0"
-# depending on what the SAI/SDK expects.
+#}
 asic_id=$(echo $OTSS_VARS | jq -r '.asic_id')
 if [ -n "$asic_id" ]
 then
     ORCHAGENT_ARGS+="-i $asic_id "
-fi
-
-# for multi asic platforms add the asic name to the record file names
-if [[ "$NAMESPACE_ID" ]]; then
-    ORCHAGENT_ARGS+="-f otss.asic$NAMESPACE_ID.rec -j otairedis.asic$NAMESPACE_ID.rec "
 fi
 
 hwsku=`sonic-cfggen -d -v "DEVICE_METADATA['localhost']['hwsku']"`
