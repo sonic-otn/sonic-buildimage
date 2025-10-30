89b80f8947d43aa011e753ea2c3381880ceb6087
kmathewm <koshy.mathew@molex.com>
2025-10-30 14:23:29 -0400

    changed libsai version to 1.1.0

--- platform/otn-kvm/sai.mk
+++ platform/otn-kvm/sai.mk
@@ -1,4 +1,4 @@
-LIBSAI_VERSION = 1.0.0
+LIBSAI_VERSION = 1.1.0
 OTN_KVM_LIBSAI_DEB = libsai-mlx-$(LIBSAI_VERSION)-amd64.deb
 $(OTN_KVM_LIBSAI_DEB)_URL = "https://raw.githubusercontent.com/sonic-molex/sonic-libmlx/refs/heads/otn/$(OTN_KVM_LIBSAI_DEB)"
 
