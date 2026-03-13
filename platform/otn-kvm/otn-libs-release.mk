697617c9fcbf2d8ec9d1b923281f411f2cd4bd33
kmathewm <koshy.mathew@molex.com>
2026-03-13 10:11:21 -0400

    fix the latest tag not working for sonic-otn-libs

--- platform/otn-kvm/otn-libs-release.mk
+++ platform/otn-kvm/otn-libs-release.mk
@@ -5,5 +5,9 @@
 # version in the filename (e.g. libsai-otn-1.2.0-amd64.deb).
 #   make OTN_LIBS_RELEASE_TAG=latest ...
 #   make OTN_LIBS_RELEASE_TAG=release-2024q1 ...
-OTN_LIBS_RELEASE_TAG ?= v1.1.0
-OTN_LIBS_RELEASE_URL = https://github.com/sonic-otn/sonic-otn-libs/releases/download/$(OTN_LIBS_RELEASE_TAG)
+OTN_LIBS_RELEASE_TAG ?=latest
+ifeq ($(OTN_LIBS_RELEASE_TAG),latest)
+OTN_LIBS_RELEASE_URL = https://github.com/sonic-molex/sonic-otn-libs/releases/latest/download
+else
+OTN_LIBS_RELEASE_URL = https://github.com/sonic-molex/sonic-otn-libs/releases/download/$(OTN_LIBS_RELEASE_TAG)
+endif
