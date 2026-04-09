cc67bc43ce975647f8d9c8856d9cc223bd8d6b73
kmathewm <koshy.mathew@molex.com>
2026-04-09 16:38:00 -0400

    fix incorrect sai.mk after rebase

--- platform/otn-kvm/sai.mk
+++ platform/otn-kvm/sai.mk
@@ -1,5 +1,5 @@
-LIBSAI_VERSION = 1.2.0
-OTN_KVM_LIBSAI_DEB = libsai-mlx-$(LIBSAI_VERSION)-amd64.deb
-$(OTN_KVM_LIBSAI_DEB)_URL = "https://raw.githubusercontent.com/sonic-molex/sonic-libmlx/refs/heads/otn/$(OTN_KVM_LIBSAI_DEB)"
+LIBSAI_VERSION = 1.3.0
+OTN_KVM_LIBSAI_DEB = libsai-otn-$(LIBSAI_VERSION)-amd64.deb
+$(OTN_KVM_LIBSAI_DEB)_URL = "$(OTN_LIBS_RELEASE_URL)/$(OTN_KVM_LIBSAI_DEB)"
 
 SONIC_ONLINE_DEBS += $(OTN_KVM_LIBSAI_DEB)
